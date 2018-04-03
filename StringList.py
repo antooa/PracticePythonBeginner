f563ae4231f2512bd0c341e44a8627286e4d3c0d
big boss <anton72325@gmail.com>
2018-04-03 14:26:53 +0300

    Improved palindrome check algorithm. Loop implementation was changed to extended slice syntax implementation.

--- StringList.py
+++ StringList.py
@@ -1,11 +1,25 @@
 # Create a program which checks if a given string is a palindrome(is read the same usually and backwards)
 
+# import time
 suspectString = input("Enter a string to palindrome check:")
-forward = []
-backward = []
-for char in suspectString:
-    forward.append(char)
-    backward.insert(0, char)
+forward = str(suspectString)
+# The code below checks what is faster: loop reverse or extended slice
+# startLoop = time.clock()
+# for char in suspectString:
+#     forward.append(char)
+#     backward.insert(0, char)
+# endLoop = time.clock()
+# loopTime = endLoop - startLoop
+# print(loopTime)
+# startRev = time.clock()
+backward = suspectString[::-1]
+# endRev = time.clock()
+# revTime = endRev - startRev
+# print(revTime)
+# print((revTime > loopTime) - (revTime < loopTime))
+# Obviously, extended slice is quicker
+# 0.00017918401514446883 - loop
+# 2.7356338189995296e-06 - slice
 if forward == backward:
     print(suspectString, "is palindrome")
 else:
