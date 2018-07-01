20eda4ee907652b62ba9dcb1a61d2a17046b5912
big boss <anton72325@gmail.com>
2018-07-01 21:20:43 +0300

    GuessingGameOne refinement

--- GuessingGameOne.py
+++ GuessingGameOne.py
@@ -1,6 +1,10 @@
 #Generate a random number between 1 and 9 (including 1 and 9).
 #Ask the user to guess the number, then tell them whether they
 #guessed too low, too high, or exactly right.
+#Extras:
+#  >Keep the game going until the user types “exit”
+#  >Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
 import random
 def main():
     print("Hey, can you guess a number between 1 and 9? Let's figure out!")
@@ -10,11 +14,16 @@ def main():
     while not if_guessed:
         guess = input('So try to guess, boy\n')
         if not guess.isnumeric():
+
+            if guess.lower() == 'exit':
+                break
+            guesses += 1
             print('Only numbers, stupid!')
             continue
         else:
             guess = int(guess)
-        guesses += 1
+            guesses += 1
+
         if guess == number:
             print("Congratulations! The number you chose was {0} and it was correct!" .format(guess))
             if_guessed = True
@@ -24,5 +33,9 @@ def main():
         else:
             print('I regret to tell you that {0} is bigger than needed!'.format(guess))
 
-    print("You tried {0} times to guess the number but finally you managed to do it!" .format(guesses))
+    if if_guessed:
+        print("You tried {0} times to guess the number but finally managed to do it!".format(guesses))
+    else:
+        print("You tried {0} times to guess the number but no soap! :(".format(guesses))
+
 main()
