268441709b5c31d4e1fc4f49403a06610a77e8f3
big boss <anton72325@gmail.com>
2018-07-02 19:26:44 +0300

    ListOfDivisors refinement

--- ListOfDivisors.py
+++ ListOfDivisors.py
@@ -1,26 +1,16 @@
 # Create a program which searches all the divisors of a number, given by user
 # from
 
+import math
+#numToFactor = int(input("Enter a number:"))
+def search_divisors(numToFactor):
+    divisors = list()
+    divisors.append(1)
+    suspects = range(2, numToFactor + 1)
+    for sus in suspects:
+        if (numToFactor % sus == 0):
+            divisors.append(sus)
 
-numToFactor = int(input("Enter a number:"))
+    return divisors
 
-suspect = 2
-divisors = [1]
-if numToFactor % 2 == 0:
-    divisors.append(2)
-    numToFactor /= 2
-    divisors.append(int(numToFactor))
-    multiplier = 2
-    while numToFactor % 2 == 0:
-        multiplier *= 2
-        divisors.append(multiplier)
-        numToFactor /= 2
-        # divisors.append(int(numToFactor))
-suspect = 3
-while numToFactor > 1:
-    if (numToFactor % suspect == 0):
-        divisors.append(suspect)
-        numToFactor /= suspect
-    else:
-        suspect += 2  # only odd
-print(divisors)
+
